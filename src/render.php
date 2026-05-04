a31550ae490d8bc7c9fbca0cc1c92d05564ba694
guzmandrade <maaguz@icloud.com>
2026-05-03 21:16:22 -0400

    Revert "remove JS view logic and enhance server-side security"
    This reverts commit e9f4ef37d23d78fb0c76848a650fa1ccf0f43987.

--- src/render.php
+++ src/render.php
@@ -7,27 +7,8 @@ if ( empty( $attributes['folderName'] ) || empty( $attributes['selectedFile'] ) 
 	return '';
 }
 
-// Validate folder and file paths
-$upload_dir = wp_upload_dir();
-$extract_base = $upload_dir['basedir'] . '/zip-extracts/';
-$folder_name = sanitize_file_name( $attributes['folderName'] );
-$selected_file = sanitize_file_name( $attributes['selectedFile'] );
-
-// Ensure paths are within allowed directory
-$full_path = $extract_base . $folder_name . '/' . $selected_file;
-$normalized_path = wp_normalize_path( $full_path );
-$normalized_extract_base = wp_normalize_path( $extract_base );
-
-if ( strpos( $normalized_path, $normalized_extract_base ) !== 0 || ! file_exists( $full_path ) ) {
-	return '';
-}
-
-// Verify file is an HTML file
-if ( ! preg_match( '/\.html?$/i', $selected_file ) ) {
-	return '';
-}
-
 $base_url = esc_url( $attributes['baseUrl'] );
+$selected_file = esc_attr( $attributes['selectedFile'] );
 $iframe_src = $base_url . '/' . $selected_file;
 $iframe_height = ! empty( $attributes['iframeHeight'] ) ? esc_attr( $attributes['iframeHeight'] ) : '600px';
 
